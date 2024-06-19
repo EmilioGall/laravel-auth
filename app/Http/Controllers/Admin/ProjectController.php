9bb401890761f71a1262e98cee6418e3d9c8424e
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-19 17:03:17 +0200

    feat: added index() method to Controller - ProjectController & created link to projects index - layouts/admin.blade

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -12,7 +13,13 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+
+        $projectsArray = Project::all();
+
+        // dd($projectsArray);
+
+        return view('projects.index', compact('projectsArray'));
+    
     }
 
     /**
---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+   <h1>Index Page</h1>
+
+@endsection
